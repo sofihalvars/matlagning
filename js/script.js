2ece14abcf7624626a3482d8239942227a6a1eae
sofihalvars <sofi.halvars@student.tabyenskilda.se>
2023-05-04 12:50:36 +0200

    v.5

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+
+
+function openNav() {
+    document.getElementById("catmenyid").style.width = "20%";
+    document.getElementById("move").style.marginLeft = "20%";
+    document.getElementById("button").style.opacity = "0";
   }
   
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
+  function closeNav() {
+    document.getElementById("catmenyid").style.width = "0";
+    document.getElementById("move").style.marginLeft= "0";
+    document.getElementById("button").style.opacity = "100%";
   }
-  
+
+
---
+++ webp/Tips_files/script.js
@@ -0,0 +1,14 @@
+
+
+function openNav() {
+    document.getElementById("catmenyid").style.width = "250px";
+    document.getElementById("move").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("catmenyid").style.width = "0";
+    document.getElementById("move").style.marginLeft= "0";
+  }
+
+
+  
